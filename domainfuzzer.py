39c32f368565c8ec22afb60648ace081a789147d
cmiller-umbc <114619506+cmiller-umbc@users.noreply.github.com>
2022-12-16 20:43:49 +0000

    Add files via upload

--- domainfuzzer.py
+++ domainfuzzer.py
@@ -10,25 +10,22 @@ from termcolor import colored, cprint
 # Set user input as domain to fuzz
 
 num_thread = 20
-'''
 domain = input("Domain: ")
 print("fuzzing top 1000 subdomains for", end=" ")
 termcolor.cprint(domain, "red", attrs=["bold"], end="")
 print("...")
-'''
+
+with open("bitquark_20160227_subdomains_popular_1000") as subdomains:
+    subdomains = subdomains.readlines()
+    
+for i in range(len(subdomains)):
+    subdomains[i] = str(subdomains[i])
+    subdomains[i] = subdomains[i].replace('\n', '')
+
 # Open bitquark top 100 subdomains and read each line into a list
 #
 # Updates are regularly made to this list on bitquark github
 # https://github.com/bitquark/dnspop
-
-with open("domains.txt") as domains:
-    domains = domains.readlines()
-
-# Scrub new line character and parse to string
-
-for i in range(len(domains)):
-    domains[i] = str(domains[i])
-    domains[i] = domains[i].replace('\n', '')
 
 # Run wget command with custom HTTP header pointed towards CloudFlare CDN instance
 termcolor.cprint("Successful subdomains:", "green",attrs=["bold"])
@@ -47,31 +44,22 @@ class thread_wget(threading.Thread):
         counter=0
         results = open("results.txt", "a")
 
-        while len(domains) > 0:
-            domain = domains.pop()
-            with open("bitquark_top_1000") as subdomains:
-                subdomains = subdomains.readlines()
-    
-            for i in range(len(subdomains)):
-                subdomains[i] = str(subdomains[i])
-                subdomains[i] = subdomains[i].replace('\n', '')
+        while len(subdomains) > 0:
+            subDomain = subdomains.pop()
+            cmd = "wget -q --connect-timeout=2 -O - -U demo http://" + subDomain + "." + domain + "/df.txt --header \"Host: domainfronter.pages.dev\""
+            sp = subprocess.Popen(str(cmd), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                universal_newlines=True)
+            rc = sp.wait()
+            out, err = sp.communicate()
 
-            while len(subdomains) > 0:
-                subDomain = subdomains.pop()
-                cmd = "wget -q --connect-timeout=2 -O - -U demo http://" + subDomain + "." + domain + "/df.txt --header \"Host: domainfronter.pages.dev\""
-                sp = subprocess.Popen(str(cmd), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                    universal_newlines=True)
-                rc = sp.wait()
-                out, err = sp.communicate()
-
-                if out == "domain fronting works!":
-                    termcolor.cprint("   " + subDomain + "." + domain, "blue")
-                    results.writelines(subDomain + "\n")
-                    counter = counter + 1
-                elif len(subdomains) == 0:
-                    break
-                elif len(subdomains) % 100 == 0:
-                    print(str(len(subdomains))+" subdomains remaining...")
+            if out == "domain fronting works!":
+                termcolor.cprint("   " + subDomain + "." + domain, "blue")
+                results.writelines(subDomain + "\n")
+                counter = counter + 1
+            elif len(subdomains) == 0:
+                break
+            elif len(subdomains) % 100 == 0:
+                print(str(len(subdomains))+" subdomains remaining...")
             
         termcolor.cprint("Total viable subdomains: ", "blue", attrs=["bold"], end="")
         termcolor.cprint(counter, "red")
